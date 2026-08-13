80cee4b1d017fc1b8a51ea6ebebdbb94d9552810
CharanKN <netala.kumar@exlservice.com>
2026-08-13 20:23:27 +0530

    Delete terraform directory

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "connect_instance_id" { value = aws_connect_instance.acme_support.id }
-output "connect_instance_arn" { value = aws_connect_instance.acme_support.arn }
-
-output "claimed_dnis_numbers" {
-  value = {
-
-  }
-}
